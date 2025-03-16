8b236061a5298ed2fd6673ace68fdfb366e698c8
NIraj Balpande <nirajbalpande12@gmail.com>
2025-03-16 18:26:52 +0530

    Disable
    what if disabled basic form login & httpbasic
    403 error

--- demo/src/main/java/com/example/demo/config/ProjectSecurityConfig.java
+++ demo/src/main/java/com/example/demo/config/ProjectSecurityConfig.java
@@ -4,24 +4,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class ProjectSecurityConfig {
 
 	@Bean
 	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-//		http.authorizeHttpRequests((requests) -> requests.anyRequest().denyAll());
+//		http.authorizeHttpRequests((requests) -> requests.anyRequest().denyAll()); 
 //		http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
 		http.authorizeHttpRequests(
 				(requests) -> requests.requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards")
-						.authenticated().requestMatchers("/notices", "/contact").permitAll());
-		http.formLogin(withDefaults());
-		http.httpBasic(withDefaults());
-//		http.formLogin(login -> login.disable()); // Disables
-//		http.httpBasic(basic -> basic.disable()); // Disables basic authentication
+						.authenticated().requestMatchers("/notices", "/contact","/error").permitAll());
+		http.formLogin(Customizer.withDefaults()); //Enable
+//		http.formLogin(flc -> flc.disable()); // Disables 
+		http.httpBasic(Customizer.withDefaults());
+//		http.httpBasic(hbc -> hbc.disable()); // Disables basic authentication
+		 
 		return http.build();
 	}
 }
